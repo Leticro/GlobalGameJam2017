a517133cabccc68adb590135bf265ae0bdf2a75d
Kevin Chen <kchen@unomaha.edu>
2017-01-21 13:46:32 -0600

    added camera restrictions

--- Assets/Scripts/Follow.cs
+++ Assets/Scripts/Follow.cs
@@ -10,17 +10,18 @@ public class Follow : MonoBehaviour
     private GameObject _player;
 
     private Vector3 camDistance = new Vector3(-8, 20, -15);
+    private float sensativity = 1 / 10;
 
     void Update()
     {
         var scroll = Input.GetAxis("Mouse ScrollWheel");
-        if (scroll > 0f)
+        if (scroll > 0f && _camera.transform.position.y > 6) //zoom in
         {
-            camDistance -= new Vector3(0, 2, 0);
+            camDistance -= new Vector3((float)-0.8, 2, (float)-1.5);
         }
         else if (scroll < 0f)
         {
-            camDistance += new Vector3(0, 2, 0);
+            camDistance += new Vector3((float)-0.8, 2, (float)-1.5); //zoom out
         }
 
         _camera.transform.position = _player.transform.position + camDistance;
