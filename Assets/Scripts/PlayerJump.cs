4eabc729141fd36b967a7e9c178ac48fc37e8a0b
Kevin Chen <kchen@unomaha.edu>
2017-01-21 19:06:48 -0600

    Player Jump enabled

--- Assets/Scripts/PlayerJump.cs
+++ Assets/Scripts/PlayerJump.cs
@@ -6,14 +6,30 @@ public class PlayerJump : MonoBehaviour
 {
     #region Private
     [SerializeField]
-    private Vector3 jump = new Vector3(0, 200, 0);
+    private float _jumpPower;
+    [SerializeField]
+    private float _acceptableSlope;
+
+    private bool _col;
     #endregion
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && _col)
         {
-            GetComponent<Rigidbody>().AddForce(jump, ForceMode.Impulse);
+            GetComponent<Rigidbody>().AddForce(new Vector3(0, _jumpPower, 0), ForceMode.Impulse);
+            _col = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        foreach (var c in col.contacts)
+        {
+            if (c.point.y > 0.5f - _acceptableSlope && c.point.y < 0.5f + _acceptableSlope)
+            {
+                _col = true;
+            }
         }
     }
 }
