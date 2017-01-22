e32629a850e61805eb9a5e0dc5cd7bf9c0505711
Kevin Chen <kchen@unomaha.edu>
2017-01-22 11:18:36 -0600

    adding scroll options

--- Assets/Scripts/Scroll.cs
+++ Assets/Scripts/Scroll.cs
@@ -4,16 +4,54 @@ using UnityEngine;
 
 public class Scroll : MonoBehaviour
 {
-	private void mouseScroll()
+    #region Private
+    [SerializeField]
+    private float _zoomInCap;
+    [SerializeField]
+    private float _zoomOutCap;
+    [SerializeField]
+    private float _cameraSensitivity;
+
+    private float _padScroll;
+
+    private readonly float CAMERA_SCALAR = 0.1f;
+    #endregion
+
+    private void Update()
     {
-        // var scroll = Input.GetAxis("Mouse ScrollWheel");
-        // if (scroll > 0f && _camera.transform.position.y > _zoomInCap)
-        // {
-        //     _camDistance -= _camSense;
-        // }
-        // else if (scroll < 0f && _camera.transform.position.y < _zoomOutCap)
-        // {
-        //     _camDistance += _camSense;
-        // }
+        float scroll = mouseScroll();
+
+        if (scroll > 0f && transform.position.y > _zoomInCap)
+        {
+            print(scroll);
+            transform.position -= transform.position * _cameraSensitivity * CAMERA_SCALAR;
+        }
+        else if (scroll < 0f && transform.position.y < _zoomOutCap)
+        {
+            print(scroll);
+            transform.position += transform.position * _cameraSensitivity * CAMERA_SCALAR;
+        }
+    }
+
+        private float mouseScroll()
+    {
+            float mouseScroll = Input.GetAxis("Mouse ScrollWheel");
+
+            if (mouseScroll != 0)
+                return mouseScroll;
+            else
+                return _padScroll;
+    }
+
+    //Get TrackPad Scroll
+    void OnGUI()
+    {
+        if (Event.current.type == EventType.ScrollWheel)
+        {
+            print(Event.current.delta);
+            _padScroll = Event.current.delta.y / 100;
+        }
+        else
+            _padScroll = 0;
     }
 }
